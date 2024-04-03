5063b01ec39b60c3092a8108477953f702e01ec7
ncthangg <t0911609811@gmail.com>
2024-04-03 14:06:43 +0700

    4/3/24 - 1st

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
+// function component
 function App() {
+  // const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coco là cục cức thúi
+          Coco là cục cức thúi trôi sông
         </p>
         <a
           className="App-link"
